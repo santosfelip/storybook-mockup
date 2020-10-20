c0c9b620e42debb3472a472402ceca8ac3de3cfd
santosfelip <nascimento.fe14@gmail.com>
2020-10-20 08:13:03 -0400

    StoryBook rodando com tests

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TesteComponent } from './teste/teste.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    TesteComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/stories/Button.stories.ts
+++ src/stories/Button.stories.ts
@@ -1,39 +1,48 @@
 // also exported from '@storybook/angular' if you can deal with breaking changes in 6.1
+import { addDecorator } from '@storybook/angular';
+import { withTests } from '@storybook/addon-jest';
 import { Story, Meta } from '@storybook/angular/types-6-0';
 import Button from './button.component';
 
+addDecorator(
+    withTests({
+        results: '',
+        filesExt: "((\\.specs?)|(\\.tests?))?(\\.ts)?$",
+    })
+)
+
 export default {
-  title: 'Example/Button',
-  component: Button,
-  argTypes: {
-    backgroundColor: { control: 'color' },
-  },
+    title: 'Example/Button',
+    component: Button,
+    argTypes: {
+        backgroundColor: { control: 'color' },
+    },
 } as Meta;
 
 const Template: Story<Button> = (args: Button) => ({
-  component: Button,
-  props: args,
+    component: Button,
+    props: args,
 });
 
 export const Primary = Template.bind({});
 Primary.args = {
-  primary: true,
-  label: 'Button',
+    primary: true,
+    label: 'Button',
 };
 
 export const Secondary = Template.bind({});
 Secondary.args = {
-  label: 'Button',
+    label: 'Button',
 };
 
 export const Large = Template.bind({});
 Large.args = {
-  size: 'large',
-  label: 'Button',
+    size: 'large',
+    label: 'Button',
 };
 
 export const Small = Template.bind({});
 Small.args = {
-  size: 'small',
-  label: 'Button',
+    size: 'small',
+    label: 'Button',
 };
