b2c99776f3cc840867c33a073bcce133bd3ab868
Alexander Danowicz <alexdanowicz@gmail.com>
2020-10-21 15:04:57 -0300

    initial commit

--- src/app/noticia/noticia.component.ts
+++ src/app/noticia/noticia.component.ts
@@ -10,7 +10,7 @@ export class NoticiaComponent {
   public image: string = 'https://images.unsplash.com/photo-1603270569894-2ff545c46ad5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1052&q=80'
   public title: string = 'BAUDUCCO ALINHA PANETTONE'
   public text: string = 'A Crispim Porter + Boguski, responsável pela comunicação da linha de panettones da Bauducco no mercado internacional, também vai responder pela estratégia publicitária no Brasil...'
-  public date = new Date();
+  public date: Date = new Date();
 
   constructor() { 
 
